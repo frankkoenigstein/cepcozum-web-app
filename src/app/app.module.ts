28649906bea268881449d7ba29bd13c4a2aee1c5
Frank Koenigstein <frank.koenigstein@posteo.de>
2017-05-20 15:21:42 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProblemsComponent } from './problems/problems.component';
+import { PhoneComponent } from './phone/phone.component';
+import { DataService } from './data.service';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemsComponent,
+    PhoneComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
